9af09104c2e227914eabbecbe9064ccc2158bdd6
karlalan <karl0385@163.com>
2015-08-26 12:09:02 +0800

    untracking dir

--- target/assets/ace/ace.widget-on-reload-45b01cdbc5ff56701c3ffcb9599a48fe.js
+++
@@ -1,1 +0,0 @@
-!function($){$(document).on("reload.ace.widget",".widget-box",function(){var $box=$(this);setTimeout(function(){$box.trigger("reloaded.ace.widget")},parseInt(1e3*Math.random()+1e3))})}(window.jQuery);
